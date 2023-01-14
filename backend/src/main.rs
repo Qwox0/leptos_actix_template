e20727b586f63498a77af0842b0b414115952afb
Qwox <qwox@qwox.com>
2023-01-14 17:06:43 +0100

    feat: add tls support

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,7 +1,8 @@
 use actix_files::Files;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-//use log::info;
-//use my_web_app::MyTestStruct;
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+
+const SOCKET_ADDRESS: &str = "0.0.0.0:33080";
 
 #[get("/hello")]
 async fn hello() -> impl Responder {
@@ -12,8 +13,13 @@ async fn hello() -> impl Responder {
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let mut ssl_builder = SslAcceptor::mozilla_intermediate(SslMethod::tls())?;
+    ssl_builder.set_private_key_file("privkey16.pem", SslFiletype::PEM)?;
+    ssl_builder.set_certificate_chain_file("fullchain16.pem")?;
+
     HttpServer::new(|| {
         App::new()
             .service(hello)
@@ -25,7 +31,7 @@ async fn main() -> std::io::Result<()> {
                 web::to(|| HttpResponse::NotFound()),
             )
     })
-    .bind("127.0.0.1:8080")?
+    .bind_openssl(SOCKET_ADDRESS, ssl_builder)?
     .run()
     .await
 }
